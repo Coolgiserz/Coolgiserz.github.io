daf651283ad966c039e2aeb09cd79ae953682201
Coolgiserz <2811159909@qq.com>
2021-01-08 11:24:56 +0800

    update sites

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,5 +1,5 @@
 (() => {
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201221153245-e47738df5972/assets/js/wowchemy-theming.js
+  // ns-hugo:/var/folders/dj/xmnsfbj9001dlp80mtmdq2400000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201221153245-e47738df5972/assets/js/wowchemy-theming.js
   const body = document.body;
   function getThemeMode() {
     return parseInt(localStorage.getItem("wcTheme") || 2);
